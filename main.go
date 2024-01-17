2fe9a988c2fa712e47a67623745904ae84f1fab7
donghli <donghli@tencent.com>
2024-01-17 23:13:18 +0800

    Add Functionalities

--- main.go
+++ main.go
@@ -4,28 +4,84 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Literank/gorep/pkg/grep"
 )
 
 func main() {
+	// Set custom usage message
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] pattern file_path\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+	}
+
+	// Optional flags
+	countFlag := flag.Bool("c", false, "count\nOnly a count of selected lines is written to standard output.")
+	ignoreCaseFlag := flag.Bool("i", false, "ignore-case\nPerform case insensitive matching. By default, it is case sensitive.")
+	lineNumberFlag := flag.Bool("n", false, "line-number\nEach output line is preceded by its relative line number in the file, starting at line 1. This option is ignored if -count is specified.")
+	recursiveFlag := flag.Bool("r", false, "recursive\nRecursively search subdirectories listed.")
+	invertMatchFlag := flag.Bool("v", false, "invert-match\nSelected lines are those not matching any of the specified patterns.")
+
 	flag.Parse()
 
 	// Retrieve positional arguments
 	// pattern - The pattern to search for
 	// file_path - The path to the file to search in
 	args := flag.Args()
-
 	if len(args) < 2 {
-		log.Fatal("Both pattern and file_path are required")
+		fmt.Println("Both pattern and file_path are required.")
+		flag.Usage()
+		os.Exit(0)
+	}
+	pattern, filePath := args[0], args[1]
+
+	options := &grep.Options{}
+	if *ignoreCaseFlag {
+		options.IgnoreCase = true
+	}
+	if *invertMatchFlag {
+		options.InvertMatch = true
 	}
 
-	result, err := grep.Grep(args[0], args[1])
-	if err != nil {
-		log.Fatal(err)
+	var result grep.MatchResult
+	var err error
+
+	if *recursiveFlag {
+		result, err = grep.GrepRecursive(pattern, filePath, options)
+		if err != nil {
+			log.Fatal("Failed to do recursive grep, error:", err)
+		}
+	} else {
+		result, err = grep.Grep(pattern, filePath, options)
+		if err != nil {
+			log.Fatal("Failed to grep, error:", err)
+		}
 	}
 
-	for _, line := range result {
-		fmt.Println(line)
+	if *countFlag {
+		fmt.Println(grep.GrepCount(result))
+	} else {
+		printResult(result, *lineNumberFlag)
+	}
+}
+
+func printResult(result grep.MatchResult, lineNumberOption bool) {
+	currentFile := ""
+	fileCount := len(result)
+
+	for filePath, items := range result {
+		for _, item := range items {
+			if fileCount > 1 && filePath != currentFile {
+				currentFile = filePath
+				fmt.Printf("\n%s:\n", filePath)
+			}
+			if lineNumberOption {
+				fmt.Printf("%d: %s\n", item.LineNumber, item.Line)
+			} else {
+				fmt.Println(item.Line)
+			}
+		}
 	}
 }
--- pkg/grep/search.go
+++ pkg/grep/search.go
@@ -3,33 +3,111 @@ package grep
 import (
 	"bufio"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
-func Grep(pattern string, filePath string) ([]string, error) {
+// MatchItem represents a match in grep searching
+type MatchItem struct {
+	LineNumber int
+	Line       string
+}
+
+// MatchResult represents all matches of all files of a grep search
+type MatchResult = map[string][]*MatchItem
+
+// Options struct represents the control options of a grep search
+type Options struct {
+	CountOnly   bool
+	IgnoreCase  bool
+	InvertMatch bool
+}
+
+func Grep(pattern string, filePath string, options *Options) (MatchResult, error) {
+	lines, err := readFileLines(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var matchingLines []*MatchItem
+	patternRegex, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	// normal grep
+	if options == nil {
+		matchingLines = filterLines(patternRegex, lines, true)
+	} else {
+		if options.IgnoreCase {
+			patternRegex, err = regexp.Compile("(?i)" + pattern)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if options.InvertMatch {
+			matchingLines = filterLines(patternRegex, lines, false)
+		} else {
+			matchingLines = filterLines(patternRegex, lines, true)
+		}
+	}
+	return MatchResult{filePath: matchingLines}, nil
+}
+
+func GrepCount(result MatchResult) int {
+	count := 0
+	for _, v := range result {
+		count += len(v)
+	}
+	return count
+}
+
+func GrepRecursive(pattern string, directoryPath string, options *Options) (MatchResult, error) {
+	results := make(MatchResult)
+	filepath.Walk(directoryPath, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if !info.IsDir() {
+			result, grepErr := Grep(pattern, filePath, options)
+			if grepErr != nil {
+				return grepErr
+			}
+			results[filePath] = result[filePath]
+		}
+		return nil
+	})
+	return results, nil
+}
+
+func readFileLines(filePath string) ([]string, error) {
+	// Open the file
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var matchingLines []string
+	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
+	var lines []string
 	for scanner.Scan() {
-		line := scanner.Text()
-		match, err := regexp.MatchString(pattern, line)
-		if err != nil {
-			return nil, err
-		}
-		if match {
-			matchingLines = append(matchingLines, strings.TrimLeft(line, " \t"))
-		}
+		lines = append(lines, scanner.Text())
 	}
-
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
+	return lines, nil
+}
 
-	return matchingLines, nil
+func filterLines(pattern *regexp.Regexp, lines []string, flag bool) []*MatchItem {
+	var filteredLines []*MatchItem
+	for lineNumber, line := range lines {
+		if flag == pattern.MatchString(line) {
+			filteredLines = append(filteredLines, &MatchItem{lineNumber + 1, strings.TrimLeft(line, " \t")})
+		}
+	}
+	return filteredLines
 }
